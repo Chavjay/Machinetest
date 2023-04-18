eb5691352714ec4b6303fbec302305ffbbb476ed
Chavjay <110823063+Chavjay@users.noreply.github.com>
2023-04-18 19:24:59 +0530

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,56 @@
+﻿
+<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+   <div>
+       <div style="font-size:large" align="center">Category</div>
+       <table class="nav-justified" style="height: 194px">
+           <tr>
+               <td class="modal-sm" style="width: 251px">&nbsp;</td>
+               <td style="width: 337px">CategoryId</td>
+               <td>
+                   <asp:TextBox ID="TextBox1" runat="server" Width="163px"></asp:TextBox>
+                   <asp:Button ID="Button6" runat="server" Font-Bold="True" OnClick="Button6_Click" Text="Get" />
+               </td>
+           </tr>
+           <tr>
+               <td class="modal-sm" style="width: 251px">&nbsp;</td>
+               <td style="width: 337px">CategoryName</td>
+               <td>
+                   <asp:TextBox ID="TextBox2" runat="server" Width="163px"></asp:TextBox>
+               </td>
+           </tr>
+           <tr>
+               <td class="modal-sm" style="width: 251px">&nbsp;</td>
+               <td style="width: 337px">&nbsp;</td>
+               <td>&nbsp;</td>
+           </tr>
+           <tr>
+               <td class="modal-sm" style="width: 251px">&nbsp;</td>
+               <td style="width: 337px">&nbsp;</td>
+               <td>
+                   &nbsp;</td>
+           </tr>
+           <tr>
+               <td class="modal-sm" style="width: 251px">&nbsp;</td>
+               <td style="width: 337px">&nbsp;</td>
+               <td>
+                   <asp:Button ID="Button1" runat="server" Font-Bold="True" OnClick="Button1_Click" Text="Insert" />
+                   <asp:Button ID="Button2" runat="server" Font-Bold="True" OnClick="Button2_Click" Text="Update" />
+                   <asp:Button ID="Button3" runat="server" Font-Bold="True" OnClick="Button3_Click" Text="Delete" />
+               </td>
+           </tr>
+           <tr>
+               <td class="modal-sm" style="width: 251px">&nbsp;</td>
+               <td style="width: 337px">&nbsp;</td>
+               <td>
+                   <asp:GridView ID="GridView1" runat="server" Height="210px" Width="584px">
+                   </asp:GridView>
+               </td>
+           </tr>
+       </table>
+       <br />
+
+   </div>
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+public partial class _Default : Page
+{
+	protected void Page_Load(object sender, EventArgs e)
+	{
+		if (!IsPostBack)
+		{
+			loadrecord();
+		}
+	}
+	SqlConnection con = new SqlConnection("Data Source=LAPTOP-OQPKJRKS\\SQLEXPRESS;Initial Catalog=newcore;Integrated Security=True");
+
+	protected void Button1_Click(object sender, EventArgs e)
+	{
+		con.Open();
+		SqlCommand cmd = new SqlCommand("Insert into Category values('" + int.Parse(TextBox1.Text) + "','" + TextBox2.Text + "')", con);
+		cmd.ExecuteNonQuery();
+		con.Close();
+		ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Inserted');", true);
+		loadrecord();
+
+	}
+	void loadrecord()
+	{
+		SqlCommand cmd = new SqlCommand("select * from Category", con);
+		SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+		DataTable dt = new DataTable();
+		adapter.Fill(dt);
+		GridView1.DataSource = dt;
+		GridView1.DataBind();
+	}
+
+	protected void Button2_Click(object sender, EventArgs e)
+	{
+		con.Open();
+		SqlCommand cmd = new SqlCommand("update Category set CategoryName='" + TextBox2.Text + "' where CategoryId='" + int.Parse(TextBox1.Text) + "'", con);
+		cmd.ExecuteNonQuery();
+		con.Close();
+		ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Updated');", true);
+		loadrecord();
+	}
+
+	protected void Button3_Click(object sender, EventArgs e)
+	{
+		con.Open();
+		SqlCommand cmd = new SqlCommand("delete Category  where CategoryId='" + int.Parse(TextBox1.Text) + "'", con);
+		cmd.ExecuteNonQuery();
+		con.Close();
+		ScriptManager.RegisterStartupScript(this, this.GetType(), "script", "alert('Successfully Deleted');", true);
+		loadrecord();
+	}
+
+
+	protected void Button5_Click(object sender, EventArgs e)
+	{
+		SqlCommand cmd = new SqlCommand("select * from Category where CategoryId='" + int.Parse(TextBox1.Text) + "'", con);
+		SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+		DataTable dt = new DataTable();
+		adapter.Fill(dt);
+		GridView1.DataSource = dt;
+		GridView1.DataBind();
+	}
+
+	protected void Button6_Click(object sender, EventArgs e)
+	{
+		con.Open();
+		SqlCommand cmd = new SqlCommand("select * from Category where CategoryId='" + int.Parse(TextBox1.Text) + "'", con);
+		SqlDataReader reader = cmd.ExecuteReader();
+		while(reader.Read())
+		{
+			TextBox2.Text=reader.GetValue(1).ToString();
+		}
+
+	}
+}
